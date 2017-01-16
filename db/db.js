1c5240a977bda69023d2f0dd0025569e54aa9910
James <james.brown9809@hotmail.co.nz>
2017-01-16 15:17:49 +1300

    created cats table

---
+++ db/db.js
@@ -0,0 +1,5 @@
+const Knex = require('knex')
+const knexConfig = require('../knexfile')[process.env.NODE_ENV || 'development']
+const knex = Knex(knexConfig)
+
+const getAllCats = () => knex
---
+++ migrations/20170116151241_cats.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('cats', table => {
+    table.increments('id')
+    table.string('name')
+    table.string('story')
+    table.integer('age')
+    table.integer('lives')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('cats')
+};
