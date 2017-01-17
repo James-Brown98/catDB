d899ae9bfba57f07820cb599db491779e6c84dc4
James <james.brown9809@hotmail.co.nz>
2017-01-17 18:43:40 +1300

    update

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const corsOptions = {
+  origin: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  credentials: true
+}
 
-var cats = require('./routes/cats');
+const cats = require('./routes/cats');
 
-var app = express();
+const app = express();
 
+app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
